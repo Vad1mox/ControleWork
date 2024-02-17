bde0017daa9272d2079bccf3d5f7379502946989
Vadimox <rucavnarucave@mail.ru>
2024-02-17 20:45:39 +0300

    Первая попытка

--- ControleWork/Program.cs
+++ ControleWork/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] words = {"мороз", "и", "солнце", "день", "чудесный"};
+
+int wordSum(string[] words)
+{
+    int wordSum = 0;
+    foreach (string e in words)
+    {
+        string word = e;
+        int letterSum = 0;
+        foreach (char i in word)
+        {
+            letterSum++;
+        }
+        if (letterSum <= 3)
+        {
+            wordSum++;
+        }
+    }
+    return wordSum;
+}
+
+string[] goal = new string[wordSum(words)];
+
+for (int i = 0; i < goal.Length; i++)
+{
+
+}
+
+string word(string[] words)
+{
+    for (int i = 0; i < words.Length; i++)
+    {
+        string word = i;
+        int sum = 0;
+        foreach (char e in word)
+        {
+            sum++;
+        }
+        if (sum <= 3)
+        {
+            return word;
+        }
+        continue ;
+    }
+}
